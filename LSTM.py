9ad276163ace0565ff19e651e3a41bfc267fc25e
phantom <p.prazbuzz@gmail.com>
2022-03-05 04:37:34 +0545

    added GRU and LSTM

--- LSTM.py
+++ LSTM.py
@@ -6,18 +6,19 @@ from seqloader import TEP
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-class RNN(nn.Module):
+class LSTM(nn.Module):
     def __init__(self, input_size, sequence_length, hidden_size, num_layers, num_classes):
-        super(RNN,self).__init__()
+        super(LSTM,self).__init__()
         self.hidden_size = hidden_size
         self.num_layers = num_layers
-        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True)
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
         self.fc = nn.Linear(hidden_size*sequence_length, num_classes)
 
     def forward(self, x):
         h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device=device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device=device)
 
-        out, _ = self.rnn(x,h0)
+        out, _ = self.lstm(x,(h0,c0))
         out = out.reshape(out.shape[0],-1)
         out = self.fc(out)
         return out
@@ -32,7 +33,7 @@ learning_rate = 0.001
 num_epochs = 1
 batch_size = 10
 
-model=RNN(input_size=input_size,sequence_length=sequence_length,hidden_size=hidden_size,num_layers=num_layers,num_classes=num_classes).to(device=device)
+model=LSTM(input_size=input_size,sequence_length=sequence_length,hidden_size=hidden_size,num_layers=num_layers,num_classes=num_classes).to(device=device)
 
 train_set = TEP(num=Type, sequence_length=sequence_length, is_train=True)
 train_loader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
