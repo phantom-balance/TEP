7c5f75c565e29faa9ea33642d6e5e43bafe6d118
phantom <p.prazbuzz@gmail.com>
2022-03-13 20:24:47 +0545

    getdata

--- getdata.py
+++ getdata.py
@@ -1,16 +1,16 @@
 import os
 import pandas as pd
 '''
-Returns Data inside a list and the labels for every time step
-of the Data in a list in that order.
+Returns Data inside a list and the labels for every time step of the Data in a list in that order.
 example:
-get_data([1,2], is_train=True) 
-will return ([data1],[labels-for-data1],[data2],[labels-for-data2])
+get_data([1], is_train=....)
 [data1]--->[d01.csv] 
 [labels-for-data1]--->[1,1,......,1]{480 labels, labeled 1}
 when is_trian=False:
 [data1]--->[d01_te.csv]
 [labels-for-data1]--->[0,0,......,1,1]{160 labels 0, 161 to 960 labeled 1}
+get_data([1,2], is_train=True) 
+will return ([data1],[labels-for-data1],[data2],[labels-for-data2])
 '''
 
 
