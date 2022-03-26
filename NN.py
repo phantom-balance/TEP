92f606dba56b833f0458015694f84a2210d7a0ce
phantom <p.prazbuzz@gmail.com>
2022-03-26 17:53:36 +0545

    NN

--- NN.py
+++ NN.py
@@ -22,16 +22,13 @@ load_model = False
 class NN(nn.Module):
     def __init__(self, input_size, num_classes):
         super().__init__()
-        self.fc1 = nn.Linear(input_size,40)
-        self.fc2 = nn.Linear(40, 30)
-        self.fc3 = nn.Linear(30, num_classes)
+        self.fc1 = nn.Linear(input_size,30)
+        self.fc2 = nn.Linear(30, num_classes)
         self.dropout = nn.Dropout(0.25)
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
-        x = self.dropout(x)
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
 
 
