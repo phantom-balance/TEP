38f8c34088f447c8d6a3754d4e9fdefd12784c4c
phantom <p.prazbuzz@gmail.com>
2022-03-26 20:17:30 +0545

    performance

--- getdata.py
+++ getdata.py
@@ -16,34 +16,44 @@ will return ([data1],[labels-for-data1],[data2],[labels-for-data2])
 
 
 def get_data(list, is_train=None):
+    noc = pd.read_csv('TEP-profbraatz-dataset/d00.csv')
+    mean = noc.mean()
+    std = noc.std()
+
     if is_train==True:
         k = []
         for idx,num in enumerate(list):
             data_path = os.path.join('TEP-profbraatz-dataset/', ('d0' if num<10 else 'd')+str(num)+".csv")
             data = pd.read_csv(data_path)
+            data_norm = data.copy()
+            for i in data_norm:
+                data_norm[i] = (data_norm[i]-mean[i])/(std[i])
             if data_path == 'TEP-profbraatz-dataset/d00.csv':
                m = []
                for i in range (500):
                    m.append(0)
-               k.extend([[data],[m]])
+               k.extend([[data_norm],[m]])
 
             else:
                 m = []
                 for i in range(480):
                     m.append(num)
-                k.extend([[data],[m]])
+                k.extend([[data_norm],[m]])
 
     else:
         k = []
         for idx,num in enumerate(list):
             data_path = os.path.join('TEP-profbraatz-dataset/', ('d0' if num<10 else 'd')+str(num)+"_te.csv")
             data = pd.read_csv(data_path)
+            data_norm = data.copy()
+            for i in data_norm:
+                data_norm[i] = (data_norm[i]-mean[i])/(std[i])
 
             if data_path == "TEP-profbraatz-dataset/d00_te.csv":
                 m = []
                 for i in range(960):
                     m.append(0)
-                k.extend([[data],[m]])
+                k.extend([[data_norm],[m]])
 
             else:
                 m = []
@@ -51,7 +61,7 @@ def get_data(list, is_train=None):
                     m.append(0)
                 for i in range(160,960):
                     m.append(num)
-                k.extend([[data],[m]])
+                k.extend([[data_norm],[m]])
 
     return k
 
