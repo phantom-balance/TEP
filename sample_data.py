d1bf535beafa79ea6663cf35c65bf9045a9aab79
phantom <p.prazbuzz@gmail.com>
2022-04-21 13:53:01 +0545

    smaller dataset creation

--- sample_data.py
+++ sample_data.py
@@ -1,21 +1,23 @@
 import pickle
 from seqloader import TEP
 from torch.utils.data import random_split
+import pickle
+
 
 Type = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21]
+sequence_length_list = [3, 4, 5, 6, 7, 8, 9]
+small_data_size = 1000
 
-# sequence_length = 5
-# train_set_all = TEP(num=Type, sequence_length=sequence_length, is_train=True)
-# test_set_all = TEP(num=Type, sequence_length=sequence_length, is_train=False)
-# small_data_size = 500
-#
-# small_test_set, _ = random_split(train_set_all, [small_data_size, len(train_set_all)-small_data_size])
-# small_train_set, _ = random_split(test_set_all, [small_data_size, len(test_set_all)-small_data_size])
-#
-# with open(f'processed_data/{sequence_length}-train_set_small{small_data_size}.p', 'wb') as f:
-#     pickle.dump(small_train_set, f)
-# with open(f'processed_data/{sequence_length}-test_set_small{small_data_size}.p', 'wb') as f:
-#     pickle.dump(small_test_set, f)
+for idx, sequence_length in enumerate(sequence_length_list):
+    all_train_set = TEP(num=Type, sequence_length=sequence_length, is_train=True)
+    all_test_set = TEP(num=Type, sequence_length= sequence_length, is_train=False)
+
+    small_test_set = random_split(all_train_set, [small_data_size, len(all_test_set)-small_data_size])
+    small_train_set, _ = random_split(all_test_set, [small_data_size, len(all_test_set)-small_data_size])
+    with open(f'processed_data/{sequence_length}-train_set_small{small_data_size}.p', 'wb') as f:
+        pickle.dump(small_train_set, f)
+    with open(f'processed_data/{sequence_length}-test_set_small{small_data_size}.p', 'wb') as f:
+        pickle.dump(small_test_set, f)
 
 
 def small_data_maker(small_data_size, sequence_length):
@@ -31,12 +33,3 @@ def small_data_maker(small_data_size, sequence_length):
         pickle.dump(small_train_set, f)
     with open(f'processed_data/{sequence_length}-test_set_small.p', 'wb') as f:
         pickle.dump(small_test_set, f)
-
-
-# small_data_maker(100, 5)
-
-'''
-arguments (custom data size, sequence length )
-this will over ride into the file  processed_data/{sequence_length}-train_set_small.p
-everytime it is executed with the current data_size argument passed
-'''
