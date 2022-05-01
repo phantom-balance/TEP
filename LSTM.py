82bb4acd40bb864a07ebb89d85f463cf5ed83e0a
phantom-balance <70420790+phantom-balance@users.noreply.github.com>
2022-05-01 23:47:45 +0545

    update

--- LSTM.py
+++ LSTM.py
@@ -43,6 +43,9 @@ model = LSTM(feature_length=feature_length,sequence_length=sequence_length,hidde
 train_set = TEP(num=Type, sequence_length=sequence_length, is_train=True)
 test_set = TEP(num=Type, sequence_length=sequence_length, is_train=False)
 
+print(f"sequence_length={sequence_length},lr={learning_rate},batch_size={batch_first}")
+print(f"num_layers={num_layers},hidden_size={hidden_size}")
+
 # small_train_set, _ = random_split(train_set, [small_data_size, len(train_set)-small_data_size])
 # small_test_set, _ = random_split(test_set, [small_data_size, len(test_set)-small_data_size])
 
